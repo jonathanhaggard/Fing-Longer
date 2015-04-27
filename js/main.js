d78b02a7544b009caaa92c395420b996903539c8
Jonathan Haggard <info@jonathanhaggard.com>
2015-04-27 06:53:58 -0700

    phaser engine

--- js/main.js
+++ js/main.js
@@ -1,67 +1,58 @@
-var $mouseX = 0, $mouseY = 0;
-var $xp = 0, $yp =0;
 
-$("body").mousemove(function(e){
-    $mouseX = e.pageX;
-    $mouseY = e.pageY;    
-});
+var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
-var $loop = setInterval(function(){
-// change 12 to alter damping higher is slower
-$xp += (($mouseX - $xp)/12);
-$yp += (($mouseY - $yp)/12);
-$(".fing").css({left:$xp +'px', top:$yp +'px'});  
-}, 0);
+function preload() {
+	game.load.image('fing', './img/fing.png');
+	game.load.image('test', './img/test.png');
+}
 
-//var img = $('.fing');
-//if(img.length > 0){
-//    var offset = img.offset();
-//    function mouse(evt){
-//        var center_x = $(window).width() / 2;
-//        var center_y = $(window).height() / 2;
-//        var mouse_x = evt.pageX; var mouse_y = evt.pageY;
-//        var radians = Math.atan2(mouse_x - center_x, mouse_y - center_y);
-//        var degree = (radians * (180 / Math.PI) * -1) + 90; 
-//        img.css('-moz-transform', 'rotate('+degree+'deg)');
-//        img.css('-webkit-transform', 'rotate('+degree+'deg)');
-//        img.css('-o-transform', 'rotate('+degree+'deg)');
-//        img.css('-ms-transform', 'rotate('+degree+'deg)');
-//    }
-//    $(document).mousemove(mouse);
-//}
-var i = 0;
+var player;
+var item;
 
-$("body").mousedown(function(e){  
-   $('.clickandhold').attr("class", "shy clickandhold");
+function create() {
+	game.stage.backgroundColor = '#fdb39c' 
+	game.physics.startSystem(Phaser.Physics.ARCADE);
+	
+	
+	
+    player = game.add.sprite(game.world.centerX, game.world.centerY, 'fing');
+    game.physics.enable(player, Phaser.Physics.ARCADE);
+	player.anchor.setTo(0.5, 0);
+	player.body.enable = true;
+	player.body.exists = true;
+	
+	
+	item = game.add.sprite(200, 200, 'test');
+	game.physics.enable(item, Phaser.Physics.ARCADE);
+	item.body.immovable = true;
+	item.body.enable = true;
+	
+	
+    game.canvas.addEventListener('mousedown', requestLock);
+    game.input.addMoveCallback(move, this);
+}
+
+function requestLock() {
+    game.input.mouse.requestPointerLock();
+}
+
+function move(pointer, x, y) {
+
+    if (game.input.mouse.locked)
+    {
+        player.x += game.input.mouse.event.webkitMovementX;
+        player.y += game.input.mouse.event.webkitMovementY;
+    }
     
-   flag = new Date().getTime();
-   $('.fing').addClass("visible, fingclick");
-   
-   addCirc = setInterval(add,1000);
-  function add(){
-       $("#TargetArea").append("<div class='New'></div>");
-       $("body").mousemove(function(e){
-           $mouseX = e.pageX;
-           $mouseY = e.pageY;    
-       });
-       $xp += (($mouseX - $xp)/12);
-       $yp += (($mouseY - $yp)/12);
-       $(".New").each(function(i, elm) {
-       
-       	$(this).append('<span class="index">' + (i + 1) + '</span>').css({left:$xp +'px', top:$yp +'px', background: Please.make_color({ golden: false, saturation: .7, value: .8}) , zIndex:  i  });  
-       });
-  }
-  
-   
-    
-}).mouseup(function(e){  
-    
-   flag2 = new Date().getTime();
-   var passed = flag2 -flag;
-   $('.fing').removeClass("visible, fingclick");
-   $("#result").html(' Golly! You lasted<span>' +passed+ '</span> milliseconds')
-   
-   clearInterval(addCirc);   
-   
-   console.log(passed, i); //time passed in milliseconds
-});
+
+}
+
+function update() {
+	game.physics.arcade.collide(player, item, collisionHandler, null, this);
+}
+
+function collisionHandler (obj1, obj2) {
+
+    game.stage.backgroundColor = '#992d2d';
+
+}
