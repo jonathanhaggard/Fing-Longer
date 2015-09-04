e733b8e4a274d3416f17233c41eef86dd7b26a47
Jonathan Haggard <info@jonathanhaggard.com>
2015-09-04 13:28:01 -0700

    Scoring system, particle generator, rings align with finger

--- js/main.js
+++ js/main.js
@@ -2,50 +2,102 @@
 var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() {
-	game.load.image('fing', './img/fing.png');
-	game.load.image('item', './img/test.png');
-	game.load.image('item2', './img/test.png');
+	game.load.image('fing', './img/fing.svg');
+	game.load.image('ring', './img/ring.svg');
 }
 
 var player;
-var item;
-var item2;
-
+var ring;
+var emitter;
+var score = 0;
+var scoreText;
+var ringParticle;
 function create() {
 	game.stage.backgroundColor = '#fdb39c' 
 	game.physics.startSystem(Phaser.Physics.ARCADE);
 	
+	//add score and ignore gravity
+	scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+	game.physics.enable(scoreText, Phaser.Physics.ARCADE);
+	scoreText.body.enable = true;
+	scoreText.body.exists = true;
+	scoreText.body.allowGravity = false;
 	
-	
+	//add finger and set the anchor
     player = game.add.sprite(game.world.centerX, game.world.centerY, 'fing');
     game.physics.enable(player, Phaser.Physics.ARCADE);
-	player.anchor.setTo(0.5, 0);
+	player.anchor.setTo(-0.25, 0);
 	player.body.enable = true;
 	player.body.exists = true;
-
-	
-	item = game.add.sprite(200, 200, 'item');
-	game.physics.enable(item, Phaser.Physics.ARCADE);
-	item.body.immovable = true;
-	item.body.enable = true;
-	item.body.collideWorldBounds = true;
-	
-    emitter = game.add.emitter(0, 0, window.innerWidth);
-
-    emitter.makeParticles('item', [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20], 200, true, true);
-
-    emitter.minParticleSpeed.setTo(-200, -300);
-    emitter.maxParticleSpeed.setTo(200, -400);
-    emitter.gravity = 150;
-    emitter.bounce.setTo(0.5, 0.5);
-	emitter.minRotation = 0;
-	emitter.maxRotation = 0;
-	
-    emitter.start(false, 8000, 1000);
+	player.body.allowGravity = false;
+	player.body.checkCollision.up = false;
+	player.body.checkCollision.down = false;
+		
+	//add emittter group
+	emitter = game.add.group();
+	//group creates multiple ring sprites
+	emitter.createMultiple(250, 'ring', 0, false);
     
-    	
+    //global gravity and enable gameworld
+    game.physics.arcade.gravity.y = 300;
+    game.physics.arcade.enable(game.world, true);
+    
+    //every 800ms execute the fire function to generate rings
+    game.time.events.loop(800, fire, this);
+    
+    
+    //mouse lock request	
     game.canvas.addEventListener('mousedown', requestLock);
     game.input.addMoveCallback(move, this);
+}
+
+function fire() {
+	
+    var ringParticle = emitter.getFirstExists(false);
+
+    if (ringParticle)
+    {
+        ringParticle.frame = game.rnd.integerInRange(0,6);
+        ringParticle.exists = true;
+        ringParticle.reset(game.world.randomX, 0);
+
+        ringParticle.body.bounce.y = -.8;
+        
+    }
+
+}
+
+//aligns the ring to the finger when it overlaps
+function align(player, ringParticle) {
+
+	//score add
+	score += 10;
+	scoreText.text = 'Score: ' + score;
+	
+	//add physics and overlap to each ring particle
+	ringParticle.body.enable = true;
+	ringParticle.x += game.input.mouse.event.movementX;
+	ringParticle.body.velocity.x = 0;	
+	
+	
+    if (ringParticle.x = (player.x))
+    {
+        return true;
+    }
+    else
+    {
+        ringParticle.body.velocity.x = player.body.velocity.x;
+        ringParticle.body.velocity.y *= -(ringParticle.body.bounce.y);
+
+        return false;
+    }
+
+}
+
+
+function bounce(player, ringParticle) {
+	ringParticle.body.enable = true;
+	ringParticle.body.velocity.x = 200;
 }
 
 function requestLock() {
@@ -55,29 +107,35 @@ function requestLock() {
 
 
 function update() {
-	game.physics.arcade.overlap(player, item, centerItem, null, this);
-	game.physics.arcade.collide(emitter, emitter, collisionHandler, null, this);
-	game.physics.arcade.overlap(player, emitter, centerItem, null, this);
+//	game.physics.arcade.overlap(player, ring, centerItem, null, this);
+	
+	
+    game.physics.arcade.overlap(player, emitter, null, align, this);
+    
+	game.physics.arcade.collide(player, ringParticle, null, bounce, this);
+	
+    emitter.forEachAlive(checkBounds, this);
 }
 
-function collisionHandler (obj1, obj2) {
+//kills rings after they go passed the viewport height
+function checkBounds(ringParticle) {
 
-    game.stage.backgroundColor = '#992d2d';
+    if (ringParticle.y > window.innerHeight)
+    {
+        ringParticle.kill();
+    }
 
 }
 
+
+//grabs the mouse and aligns the finger to it
 function move(pointer, x, y) {
 
     if (game.input.mouse.locked)
     {
-        player.x += game.input.mouse.event.webkitMovementX;
-        player.y += game.input.mouse.event.webkitMovementY;
+        player.x += game.input.mouse.event.movementX;
+        player.y += game.input.mouse.event.movementY;
     }
     
 
-}
-function centerItem (obj1, obj2){
-	    item.x += game.input.mouse.event.webkitMovementX;
-	    item.body.velocity.y = 1000;
-	    item.body.enable = true;
 }
