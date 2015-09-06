f2b833c06f6b2a65ecf830cfcd68c09a1a781a85
Jonathan Haggard <info@jonathanhaggard.com>
2015-09-05 23:34:08 -0700

    Side to side collisions, new background and new artwork for ring

--- js/main.js
+++ js/main.js
@@ -1,38 +1,90 @@
 
 var game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
+//  The Google WebFont Loader will look for this object, so create it before loading the script.
+//WebFontConfig = {
+//
+//    //  'active' means all requested fonts have finished loading
+//    //  We set a 1 second delay before calling 'createText'.
+//    //  For some reason if we don't the browser cannot render the text the first time it's created.
+//    active: function() { game.time.events.add(Phaser.Timer.SECOND, createText, this); },
+//
+//    //  The Google Fonts we want to load (specify as many as you like in the array)
+//    google: {
+//      families: ['Nunito']
+//    }
+//
+//};
+
 function preload() {
 	game.load.image('fing', './img/fing.svg');
-	game.load.image('ring', './img/ring.svg');
+	game.load.image('ring', './img/ring.png');
+	game.load.image('Bbarrier', './img/Bbarrier.png');
+//	game.load.script('webfont', '//ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js');
 }
 
+var halfwindowheight = (window.innerHeight)/2;
 var player;
+var playerLeft;
+var playerRight;
 var ring;
 var emitter;
 var score = 0;
 var scoreText;
 var ringParticle;
+
 function create() {
-	game.stage.backgroundColor = '#fdb39c' 
+	game.stage.backgroundColor = '#30BCED' 
 	game.physics.startSystem(Phaser.Physics.ARCADE);
 	
 	//add score and ignore gravity
-	scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+	scoreText = game.add.text(game.world.centerX, halfwindowheight /2, 'score', { fontSize: '10vw', fill: '#C2F970' });
 	game.physics.enable(scoreText, Phaser.Physics.ARCADE);
 	scoreText.body.enable = true;
 	scoreText.body.exists = true;
 	scoreText.body.allowGravity = false;
+	scoreText.font = 'Nunito';
+	scoreText.align = 'left';
+	scoreText.anchor.set(0.5);
 	
 	//add finger and set the anchor
     player = game.add.sprite(game.world.centerX, game.world.centerY, 'fing');
     game.physics.enable(player, Phaser.Physics.ARCADE);
-	player.anchor.setTo(-0.25, 0);
+	player.anchor.setTo(-0.30, 0);
+	player.body.collideWorldBounds=true;
+	player.body.setSize(60, halfwindowheight);
 	player.body.enable = true;
 	player.body.exists = true;
 	player.body.allowGravity = false;
 	player.body.checkCollision.up = false;
-	player.body.checkCollision.down = false;
-		
+	player.body.checkCollision.left = true;
+	player.body.checkCollision.right = true;
+	
+	playerLeft = game.add.sprite(game.world.centerX, game.world.centerY, 'Bbarrier');
+	game.physics.enable(playerLeft, Phaser.Physics.ARCADE);
+	player.body.collideWorldBounds=true;
+	playerLeft.body.collideWorldBounds=false;
+	playerLeft.body.setSize(2, halfwindowheight);
+	playerLeft.body.enable = true;
+	playerLeft.body.exists = true;
+	playerLeft.body.allowGravity = false;
+	playerLeft.body.checkCollision.up = false;
+	playerLeft.body.checkCollision.left = true;
+	playerLeft.body.checkCollision.right = false;
+	
+	playerRight = game.add.sprite(game.world.centerX, game.world.centerY, 'Bbarrier');
+	game.physics.enable(playerRight, Phaser.Physics.ARCADE);
+	player.body.collideWorldBounds=true;
+	playerRight.body.collideWorldBounds=false;
+	playerRight.body.setSize(2, halfwindowheight);
+	playerRight.body.enable = true;
+	playerRight.body.exists = true;
+	playerRight.body.allowGravity = false;
+	playerRight.body.checkCollision.up = false;
+	playerRight.body.checkCollision.left = false;
+	playerRight.body.checkCollision.right = true;
+	
+	
 	//add emittter group
 	emitter = game.add.group();
 	//group creates multiple ring sprites
@@ -50,7 +102,10 @@ function create() {
     game.canvas.addEventListener('mousedown', requestLock);
     game.input.addMoveCallback(move, this);
 }
+function render () {
 
+
+}
 function fire() {
 	
     var ringParticle = emitter.getFirstExists(false);
@@ -60,9 +115,9 @@ function fire() {
         ringParticle.frame = game.rnd.integerInRange(0,6);
         ringParticle.exists = true;
         ringParticle.reset(game.world.randomX, 0);
-
+        ringParticle.game.physics.enable(this, Phaser.Physics.ARCADE);
+//		ringParticle.body.setSize(20, 10, 0, 0);
         ringParticle.body.bounce.y = -.8;
-        
     }
 
 }
@@ -72,13 +127,14 @@ function align(player, ringParticle) {
 
 	//score add
 	score += 10;
-	scoreText.text = 'Score: ' + score;
+	scoreText.text =  score;
 	
 	//add physics and overlap to each ring particle
 	ringParticle.body.enable = true;
 	ringParticle.x += game.input.mouse.event.movementX;
 	ringParticle.body.velocity.x = 0;	
-	
+	ringParticle.body.velocity.y = 1000;
+	ringParticle.body.bounce.y = -.3;
 	
     if (ringParticle.x = (player.x))
     {
@@ -95,9 +151,21 @@ function align(player, ringParticle) {
 }
 
 
-function bounce(player, ringParticle) {
+function bounceLeft(playerLeft, ringParticle) {
 	ringParticle.body.enable = true;
-	ringParticle.body.velocity.x = 200;
+	ringParticle.body.bounce.x = 8;
+	ringParticle.body.velocity.x = -2000;
+	playerLeft.body.immovable = true;
+	console.log("bounce");
+}
+
+function bounceRight(playerRight, ringParticle) {
+	ringParticle.body.enable = true;
+	ringParticle.body.bounce.x = 8;
+	ringParticle.body.velocity.x = 2000;
+	
+	playerRight.body.immovable = true;
+	console.log("bounce");
 }
 
 function requestLock() {
@@ -110,9 +178,11 @@ function update() {
 //	game.physics.arcade.overlap(player, ring, centerItem, null, this);
 	
 	
-    game.physics.arcade.overlap(player, emitter, null, align, this);
+    game.physics.arcade.collide(player, emitter, null, align, this);
     
-	game.physics.arcade.collide(player, ringParticle, null, bounce, this);
+	game.physics.arcade.collide(playerLeft, emitter, null, bounceLeft, this);
+	
+	game.physics.arcade.collide(playerRight, emitter, null, bounceRight, this);
 	
     emitter.forEachAlive(checkBounds, this);
 }
@@ -134,7 +204,12 @@ function move(pointer, x, y) {
     if (game.input.mouse.locked)
     {
         player.x += game.input.mouse.event.movementX;
-        player.y += game.input.mouse.event.movementY;
+//        player.y += game.input.mouse.event.movementY;
+        playerLeft.x = player.x - 5;
+        playerLeft.y = player.y + 20;
+        
+        playerRight.x = player.x + 80;
+        playerRight.y = player.y + 20;
     }
     
 
